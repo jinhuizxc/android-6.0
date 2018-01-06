7b56d9c1989c928b63b92a5752d6939356d03670
jinhuizxc <1004260403@qq.com>
2018-01-06 16:15:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jinhui/android_60/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.jinhui.android_60;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * android 6.0权限demo
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
